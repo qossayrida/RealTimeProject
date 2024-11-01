8f7346230dd4f4d627699ab9f6d263e2e8b8aee2
qossayrida <aqusay06@gmail.com>
2024-11-02 00:20:43 +0200

    Print config info in src/main.c

--- Project1/src/main.c
+++ Project1/src/main.c
@@ -14,9 +14,16 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    printf("Configuration Loaded:\n");
     printf("Max Score: %d\n", config.max_score);
     printf("Max Time: %d\n", config.max_time);
-    // Initialize other processes and start game logic
+    printf("Initial Energy - Team A: %d\n", config.initial_energy_team_a);
+    printf("Initial Energy - Team B: %d\n", config.initial_energy_team_b);
+    printf("Initial Energy - Team C: %d\n", config.initial_energy_team_c);
+    printf("Stabilization Time Min: %d\n", config.stabilization_time_min);
+    printf("Stabilization Time Max: %d\n", config.stabilization_time_max);
+    printf("Pull Time Min: %d\n", config.pull_time_min);
+    printf("Pull Time Max: %d\n", config.pull_time_max);
 
     return 0;
 }
